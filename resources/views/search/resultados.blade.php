66913ea246778cb073c28e848332d46dd9526d38
nicolasblancom <nicolasblancom@gmail.com>
2016-01-22 14:17:40 +0100

    Fix mensaje 'sin resultados'

--- resources/views/search/resultados.blade.php
+++ resources/views/search/resultados.blade.php
@@ -5,7 +5,7 @@
 	<div class="row">
 		<div class="col-lg-12">
 			@if(!$users->count())
-				p{No hay resultados...}
+				<p>No hay resultados...</p>
 			@else
 				@foreach($users as $user)
 					@include('user.partials._userblock')
