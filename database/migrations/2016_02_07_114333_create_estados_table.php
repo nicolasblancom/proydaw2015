b1ea586837844c93171199ba22066c604a9a7885
nicolasblancom <nicolasblancom@gmail.com>
2016-02-07 11:56:28 +0100

    Timeline: migracion de tablas necesarias

---
+++ database/migrations/2016_02_07_114333_create_estados_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEstadosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('estados', function(Blueprint $table){
+            $table->increments('id');
+            $table->integer('usuario_id');
+            $table->integer('padre_id');
+            $table->text('body');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('estados');
+    }
+}
