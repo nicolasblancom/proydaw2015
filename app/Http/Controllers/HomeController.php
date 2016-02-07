17e6644568179afe3d8ba203b65292578d9b5e9e
nicolasblancom <nicolasblancom@gmail.com>
2016-02-07 17:50:14 +0100

    Timeline: mostrar estados propios y de amigos

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace Socialdaw\Http\Controllers;
 
 use Auth;
+use Socialdaw\Models\Status;
 
 class HomeController extends Controller
 {
@@ -15,7 +16,17 @@ class HomeController extends Controller
 
 		// Si estoy logeado, muestro el timeline o muro
 		if (Auth::check()) {
-			return view('timeline.index');
+			// Recojo mis estados y los de mis amigos, ordeno y pagino
+			$estados = Status::where(function($query){
+				return $query
+					->where('usuario_id', Auth::user()->id)
+					->orWhereIn('usuario_id', Auth::user()->amigos()->lists('id'));
+			})
+				->orderBy('created_at', 'desc')
+				->paginate(7);
+
+			return view('timeline.index')
+				->with('estados', $estados);
 		}
 
 		return view('home');
--- resources/views/timeline/index.blade.php
+++ resources/views/timeline/index.blade.php
@@ -18,7 +18,61 @@
 	</div>
 	<div class="row">
 		<div class="col-lg-5">
-			<!-- Estados del muro y respuestas -->
+			<!-- Timeline -->
+			@if(!$estados->count())
+				<p>No hay nada en tu muro aun...</p>
+			@else
+				@foreach($estados as $estado)
+					<div class="media">
+						<a
+							href="{{ route('profile.index', ['username' => $estado->usuario->username]) }}"
+							class="pull-left">
+							<img
+								src="{{ $estado->usuario->getAvatarUrl() }}"
+								alt="{{ $estado->usuario->getNombreCompletoOUsername() }}"
+								class="media-object">
+						</a>
+						<div class="media-body">
+							<h4 class="media-heading">
+								<a href="{{ route('profile.index', ['username' => $estado->usuario->username]) }}">
+									{{ $estado->usuario->getNombreCompletoOUsername() }}
+								</a>
+							</h4>
+							<p>{{ $estado->body }}</p>
+							<ul class="list-inline">
+								<li>{{ $estado->created_at->diffForHumans() }}</li>
+								<li><a href="#">Like</a></li>
+								<li>25 likes</li>
+							</ul>
+
+							<!--<div class="media">
+								<a href="#" class="pull-left">
+									<img src="" alt="" class="media-object">
+								</a>
+								<div class="media-body">
+									<h5 class="media-heading"><a href="">Juan</a></h5>
+									<p>Sí que es verdad!</p>
+									<ul class="list-inline">
+										<li>Hace 30 minutos</li>
+										<li><a href="#">Like</a></li>
+										<li>25 likes</li>
+									</ul>
+								</div>
+							</div>-->
+
+							<form action="#" role="form" method="post">
+								<div class="form-group">
+									<textarea name="respuesta-1" rows="2" class="form-control" placeholder="Responder a este estado"></textarea>
+								</div>
+								<input type="submit" value="Responder" class="btn btn-default btn-sm">
+							</form>
+						</div>
+					</div>
+				@endforeach
+
+				<!-- paginacion -->
+				{{ $estados->render() }}
+			@endif
 		</div>
 	</div>
 @stop
