98e95616fd7e4d1d01e319fe2211cf3ab25536b4
nicolasblancom <nicolasblancom@gmail.com>
2016-02-11 21:26:03 +0100

    Likes: migracion de tablas necesarias (bd)

---
+++ database/migrations/2016_02_11_211706_create_gustable_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGustableTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // gustable_tipo: el modelo a dar me gusta
+        // gustable_id: CP de la tabla correspondiente al modelo
+        Schema::create('gustable', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('usuario_id');
+            $table->integer('gustable_id');
+            $table->string('gustable_tipo');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('gustable');
+    }
+}
