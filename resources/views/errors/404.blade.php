4b23258ffbb785fd705aa5048e37349f0c24e371
nicolasblancom <nicolasblancom@gmail.com>
2016-02-13 15:13:45 +0100

    pagina de error 404

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,6 @@
+@extends('templates.default')
+
+@section('content')
+	<h3>La página que quieres ver no existe...</h3>
+	<p>Puedes volver al <a href="{{ route('home') }}">Inicio</a> y probar con otra página</p>
+@stop
