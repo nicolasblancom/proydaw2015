d80efe57e29e8be6c35ceb6f3a8926e346665753
nicolasblancom <nicolasblancom@gmail.com>
2016-04-06 19:13:01 +0200

    frontend: agrego footer sticky abajo al fondo

--- resources/views/templates/default.blade.php
+++ resources/views/templates/default.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="es">
+<html lang="es" style="position:relative; min-height: 100%;">
 	<head>
 		<meta charset="UTF-8" />
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -10,14 +10,16 @@
 		<link rel="stylesheet" href="/output/css/all.css">
 		<script src="/output/js/all.js"></script>
 	</head>
-	<body>
+	<body style="margin-bottom: 60px;">
+		<div class="mainWrapper">
+			@include('templates.partials._navigation')
 
-		@include('templates.partials._navigation')
+			<div class="container" style="padding-bottom:45px;">
+				@include('templates.partials._messages_flash')
+				@yield('content')
+			</div>
 
-		<div class="container">
-			@include('templates.partials._messages_flash')
-			@yield('content')
+			@include('templates.partials._footer')
 		</div>
-
 	</body>
 </html>
---
+++ resources/views/templates/partials/_footer.php
@@ -0,0 +1,5 @@
+<footer class="footer" style="position:absolute;bottom:0;width:100%;">
+	<div class="container">
+		<p class="text-muted">Socialdaw: Proyecto Final del CFGS DAW</p>
+	</div>
+</footer>
